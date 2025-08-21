7cfe547686a3d7e823378c701bbc55a99d145fe5
hdz-kevin <kevinhdez592@gmail.com>
2025-08-20 19:43:44 -0600

    add PlayerController tests: index, show, store

--- tests/Feature/PlayerTest.php
+++ tests/Feature/PlayerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Player;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,21 +16,80 @@ class PlayerTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * Test: enpoind /players
+     * Test: GET /players returns status 200.
      */
-    public function test_index_PlayerController_getPlayers_ReturnStatus200(): void
+    public function test_index_player_controller_get_players_return_status_200(): void
     {
         $ver = env('APP_VER');
-        $response = $this->get("/{$ver}/players");
 
+        $response = $this->get("/{$ver}/players");
         $response->assertStatus(200);
     }
 
     /**
-     * Test route that does not exist.
+     * Test: GET /players returns all players.
      */
-    public function test_non_existing_route_returns_404(): void
+    public function test_index_player_controller_returns_all_players(): void
     {
-        $this->get('/random/route')->assertStatus(404);
+        $ver = env('APP_VER');
+        $players = Player::factory()->count(13)->create();
+
+        $response = $this->get("/{$ver}/players");
+        $response->assertStatus(200)
+                 ->assertJsonCount($players->count());
+    }
+
+    /**
+     * Test: GET /players/{id} returns player by ID.
+     */
+    public function test_show_player_controller_get_player_by_id(): void
+    {
+        $ver = env('APP_VER');
+        $player = Player::factory()->create();
+
+        $response = $this->get("/{$ver}/players/{$player->id}");
+        $response->assertStatus(200)
+                 ->assertJson([
+                     'id' => $player->id,
+                     'first_name' => $player->first_name,
+                     'last_name' => $player->last_name,
+                 ]);
+    }
+
+    /**
+     * Test: POST /players returns status 422 when data is invalid.
+     */
+    public function test_store_player_controller_invalid_data_returns_status_422(): void
+    {
+        $ver = env('APP_VER');
+        $invalidPlayer = [
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'birth_date' => '1990-01-01',
+        ];
+
+        $response = $this->postJson("/{$ver}/players", $invalidPlayer);
+        $response->assertStatus(422)
+                 ->assertJsonValidationErrors(['gender']);
+    }
+
+    /**
+     * Test: POST /players creates a new player.
+     */
+    public function test_store_player_controller_create_new_player(): void
+    {
+        $ver = env('APP_VER');
+        $player = [
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'gender' => 'male',
+            'birth_date' => '1990-01-01',
+        ];
+
+        $response = $this->postJson("/{$ver}/players", $player);
+        $response->assertStatus(201)
+                 ->assertJsonFragment($player);
+
+        $this->assertDatabaseCount('players', 1);
     }
 }
