556568ad773615821096c3f4c351441f60999cd5
hdz-kevin <kevinhdez592@gmail.com>
2026-01-24 16:53:36 -0600

    API Rest routing

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+
+Route::get('/players', fn () => 'Players List...');
