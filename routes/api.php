c9b564c689d39ac4b978638fd8c0728d5e591f46
hdz-kevin <kevinhdez592@gmail.com>
2026-01-31 16:01:42 -0600

    add middleware to set request Accept header to application/json

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PlayerController;
+use App\Http\Middleware\ApiForceAcceptHeader;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
-Route::get('/players', [PlayerController::class, 'index']);
+Route::get('/players', [PlayerController::class, 'index'])->middleware([ApiForceAcceptHeader::class]);
