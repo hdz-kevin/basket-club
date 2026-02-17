52379361d41ab85c406c7b1c818f6419ecd1b49c
hdz-kevin <kevinhdez592@gmail.com>
2026-02-17 15:30:31 -0600

    add MedicalRecord model and relate it to Player

---
+++ app/Models/MedicalRecord.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class MedicalRecord extends Model
+{
+    /**
+     * Get the player that owns the medical record.
+     */
+    public function player(): BelongsTo
+    {
+        return $this->belongsTo(Player::class);
+    }
+}
--- app/Models/Player.php
+++ app/Models/Player.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Player extends Model
 {
@@ -15,4 +16,12 @@ class Player extends Model
         'gender',
         'birthdate',
     ];
+
+    /**
+     * Get the player's medical record.
+     */
+    public function medicalRecord(): HasOne
+    {
+        return $this->hasOne(MedicalRecord::class);
+    }
 }
