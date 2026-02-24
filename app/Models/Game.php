694cfef6de139a56405eeb6fb7b170a9830f1d39
Tomas Hernandez <kevinhdez592@gmail.com>
2026-02-24 17:37:40 -0600

    implement Game <-> Team relationship methods

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Game extends Model
 {
@@ -17,4 +18,12 @@ class Game extends Model
         'opposing_team_score',
         'date',
     ];
+
+    /**
+     * Get the team that owns the game
+     */
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
 }
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Team extends Model
 {
@@ -14,4 +15,12 @@ class Team extends Model
         'category',
         'gender',
     ];
+
+    /**
+     * Get the games for the team
+     */
+    public function games(): HasMany
+    {
+        return $this->hasMany(Game::class);
+    }
 }
