fa82d02f0ab3aad1648ece9d526778ac21610057
Tomas Hernandez <kevinhdez592@gmail.com>
2026-03-10 16:19:52 -0600

    add route to get the medicalrecord associated with a user through a player

--- app/Http/Controllers/QueriesController.php
+++ app/Http/Controllers/QueriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Team;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class QueriesController extends Controller
@@ -29,5 +30,16 @@ class QueriesController extends Controller
         }
 
         return response()->json($team->load('games'), 200);
+    }
+
+    public function userMedicalRecord(int $id)
+    {
+        $user = User::with('playerMedicalRecord')->find($id);
+
+        if (! $user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json($user, 200);
     }
 }
